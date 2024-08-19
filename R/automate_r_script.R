433e11c3fe48e446216d1b15f1d0cfc96138adbd
ForraForra <ludvigforsmark@gmail.com>
2024-08-19 15:57:30 +0200

    Updated

--- R/automate_r_script.R
+++ R/automate_r_script.R
@@ -1,24 +1,8 @@
 
-###########################################################
-###########################################################
-## Automate R scripts with GitHub Actions: Deploy a model
-###########################################################
-###########################################################
+## Automate R scripts with GitHub Actions
 
 
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-## Task 7: Load the readmission model
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-
-
-##---------------------------------------------------------------------------##
-## Load required packages and readmission model
-##---------------------------------------------------------------------------##
-
-## Load required packages
-
+## Hämtar paket och modell
 library(googlesheets4)
 library(readr)
 library(dplyr)
@@ -30,154 +14,147 @@ library(emayili)
 library(gargle)
 library(tidyr)
 
-
-## Load the prediction model
-
-
-##---------------------------------------------------------------------------##
-## Decrpyt authorization to Google Drive and Google Sheet
-##---------------------------------------------------------------------------##
-
-## googledrive De-auth setup
+readmit_model <- read_rds("inst/final_readmission_model.rds")
 
 
-## googlesheet De-auth setup
+## Dekryptera authorization till Google Drive and Google Sheet
+
+#googledrive setup
+googledrive::drive_auth(
+  path = gargle::secret_decrypt_json("inst/googledrive_encrypt.json", 
+                                     "GOOGLEDRIVE_KEY")
+)
+#googlesheet setup
+googlesheets4::gs4_auth(
+  path = gargle::secret_decrypt_json("inst/googledrive_encrypt.json", 
+                                     "GOOGLEDRIVE_KEY")
+)
 
 
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-## Task 8: Load the data and make predictions
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-
-##---------------------------------------------------------------------------##
-## Read in patient data and make predictions 
-##---------------------------------------------------------------------------##
-
-## Load the googlesheet where data is stored
+## Hämta data från sheets 1 och skapa prediktioner för alla raderna 
 
 
-## Load the patient raw data from the first sheet
+# Ansluter google sheeten
+sheet_id <- "https://docs.google.com/spreadsheets/d/1mDT8Pe-PtE__SxXs2dCaLttRMB1XFYk2PoUSwZuPAfU/edit?usp=sharing"
+
+patient_data <- read_sheet(sheet_id, sheet = 1)
+patient_data_with_pred <- read_sheet(sheet_id, sheet = 2)
+
+#Prediktionerna tsm med tidigare kolumner pullas till sheet 2
+pred_data <- 
+  augment(readmit_model, patient_data) |> 
+  select(all_of(colnames(patient_data)), 
+         "Prediction" = ".pred_class", 
+         "Prob No Readmit" = ".pred_No", 
+         "Prob Readmit" = ".pred_Yes")
+sheet_write(ss = sheet_id, data = pred_data, sheet = "Patient Data with Prediction")
+
+# Rad 44 till 50 bör läggas "#" för raderna, då det annars kmr komma med i automatiseringen senare 
 
 
-## Read the second sheet that will contain the patient data and prediction
-
-
-## Make predictions for the patient data (sheet 1)
-
-
-## Save/write the prediction on the second sheet
-
-
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-## Task 9: Format prediction columns as percentages
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-
-##---------------------------------------------------------------------------##
-## Helper function to format the N and O columns as percentages
-##---------------------------------------------------------------------------##
-
-## Helper function to help format googlesheet (0.456 -> 45%)
+## Formatterar prediktionsvärdena till procent (0.456 -> 45%)
 format_prediction_to_percent <- function(sheet, pos){
-  
-  ## Get the Google Sheet
   x <- gs4_get(sheet)
-  
-  ## Determine the targeted sheet
+
   range_spec <- googlesheets4:::as_range_spec(
     pos,
     sheet = 2,
     sheets_df = x$sheets, nr_df = x$named_ranges
   )
   
-  ## Form request
   range_req <- googlesheets4:::as_GridRange(range_spec)
   
-  ## Specify the cell formatting
+  cell_req <- list(
+    userEnteredFormat = list(
+      numberFormat = list(
+        type = "NUMBER", 
+        pattern = "0%"
+      )
+    )
+  )
+  field_req <- "userEnteredFormat.numberFormat"
   
-  
-  ## Generate and send the API request
-  
-  
-  
-  ## Execute the request and send the response
+  #request_generated() genererar en google sheets API
+  req <- request_generated(
+    "sheets.spreadsheets.batchUpdate", 
+    params = list(
+      spreadsheetId = as_sheets_id(sheet), 
+      requests = list(
+        repeatCell = list(
+          range = range_req, 
+          cell = cell_reg, 
+          fields = field_req
+        )
+      )
+    )
+  )
   resp_raw <- request_make(req)
   gargle::response_process(resp_raw)
 }
 
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-## Task 10: Write an automation script for prediction
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
 
-##---------------------------------------------------------------------------##
-## Automated script for prediction
-##---------------------------------------------------------------------------##
+## Automation script för prediktion
 
-## Capture the data of new patients in sheet 1
+# Prediktionerna skapas med readmission modellen
+# Skrpitet ska automatiskt utföra en prediktion (till sheets) när en ny patient-rad läggs in. 
 
-
-
-## If there is a new patient, then make prediction and 
-## append to sheet 2
+#Kod från rad 37 behövs köras innan nedan (givet rad 44 körts en gång på de "gamla" raderna):
+new_patient <- patient_data |> 
+  anti_join(patient_data_with_pred |> 
+              select("patient_id"))
 
 if(nrow(new_patients) > 0) {
-  
-  ## Apply model on new patient data
   pred_data <-
     augment(readmit_model, new_patients) |>
     select(all_of(colnames(new_patients)),
            "Prediction" = ".pred_class",
            "Prob No Readmit" = ".pred_No",
            "Prob Readmit" = ".pred_Yes")
-  
-  ## Instead of overwriting the data, we need to append new data
-  
-  ## Before appending, let's confirm that the column names are still the same.
-  ## They should be, but it best practice to confirm.
+
   
   if(!all(colnames(patient_data_with_pred) == colnames(pred_data))) {
     stop("Patient data and prediction database don't match")
   }
   
-  ## Append new patient and prediction to sheet 2
   
+  sheet_append(ss = sheet_id, 
+               data = pred_data, 
+               sheet = "Patient Data with Prediction")
   
-  ## Format the text col (i.e 0.564 -> 56%)
-  
+  format_prediction_to_percent(sheet_id, pos = "N")
+  format_prediction_to_percent(sheet_id, pos = "O")
   
 }
 
 
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-## Task 12: Write automation script to send emails
-##---------------------------------------------------------------------------##
-##---------------------------------------------------------------------------##
-
-##---------------------------------------------------------------------------##
-## Automation script for emails
-##---------------------------------------------------------------------------##
+## Skriptet som automatiskt ska skicka emails
 
 if(nrow(new_patients) > 0){
-  ## Prepare email message
   smtp <- emayili::server(
     host = "smtp.gmail.com",
     port = 465,
     username = Sys.getenv("GMAIL_USERNAME"),
     password = Sys.getenv("GMAIL_PASSWORD")
   )
+  send_to <- c("exempelmail@gmail.com")
   
-  send_to <- c("")
+  #SKapar emailet
+  emayili <- envelope() %>%
+    from("ludvigforsmark@gmail.com") %>%
+    to(send_to) %>%
+    subject("New Readmission Prediction") %>%
+    emayili::render(
+      input = "R/automate_r_email_content.Rmd", 
+      params = list(
+        patient_id = pred_data$patient_id, 
+        pred = pred_data$Prediction, 
+        no_readmit = pred_data$`Prob No Readmit`, 
+        yes_readmit = pred_data$`Prob Readmit`, 
+      ), 
+      squish = F, 
+      include_css = "bootstrap"
+    )
   
-  ## Create and send email message
-  
-  
-  ## Show as the email sends
   smtp(emayili, verbose = TRUE)
 }
-
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # automate_r_scripts
 Automation i R projekt
+
+Inte helt färdigställt då jag inte tänkt sätta de i produktion. 
